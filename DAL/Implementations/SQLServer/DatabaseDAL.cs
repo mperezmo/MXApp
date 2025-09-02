c73080d19f80d8d98fff40054124d6877e52f0cc
mperezmo <134961458+mperezmo@users.noreply.github.com>
2025-09-01 21:12:23 -0300

    Update DatabaseDAL.cs

--- DAL/Implementations/SQLServer/DatabaseDAL.cs
+++ DAL/Implementations/SQLServer/DatabaseDAL.cs
@@ -82,37 +82,96 @@ WITH INIT;";
         /// <summary>
         /// Recupera la lista de bases de datos (excluyendo las del sistema) de la instancia.
         /// </summary>
-        public static List<DatabaseInfo> GetDatabasesInfo(string instanceName, IDatabaseConnectionStrategy connectionStrategy)
+        public static List<DatabaseInfo> GetDatabases(
+            string instanceName,
+            IDatabaseConnectionStrategy connectionStrategy,
+            bool includeStatus = false,
+            bool includeSize = false,
+            bool includeBackups = false)
         {
             var connectionContext = new DatabaseConnectionContext(connectionStrategy);
-
+            var databases = new List<DatabaseInfo>();
+            
             using (var connection = connectionContext.GetConnection(instanceName))
             {
                 try
                 {
-                    connection.Open();
+                    bool includeDetails = includeStatus || includeSize || includeBackups;
+                    string query;
 
-                    string query = @"
-SELECT 
-    name AS DatabaseName
-FROM sys.databases
-WHERE database_id > 4;"; // Excluye bases de datos del sistema
+                    if (includeDetails)
+                    {
+                        query = @"
+                SELECT
+                    db.name AS DatabaseName,
+                    db.state_desc AS Status,
+                    db.recovery_model_desc AS RecoveryModel,
+                    SUM(mf.size * 8 / 1024) AS SizeMB,
+                    mf.name AS FileName,
+                    mf.physical_name AS FileLocation,
+                    (SELECT MAX(backup_finish_date)
+                     FROM msdb.dbo.backupset
+                     WHERE backupset.database_name = db.name AND backupset.type = 'D') AS LastFullBackup,
+                    (SELECT MAX(backup_finish_date)
+                     FROM msdb.dbo.backupset
+                     WHERE backupset.database_name = db.name AND backupset.type = 'L') AS LastLogBackup
+                FROM
+                    sys.databases db
+                LEFT JOIN
+                    sys.master_files mf ON db.database_id = mf.database_id
+                WHERE db.database_id > 4
+                GROUP BY
+                    db.name, db.state_desc, db.recovery_model_desc, mf.name, mf.physical_name
+                ORDER BY
+                    db.name;";
+                    }
+                    else
+                    {
+                        query = @"
+                SELECT
+                    name AS DatabaseName
+                FROM sys.databases
+                WHERE database_id > 4;";
+                    }
+
+                    connection.Open();
 
                     using (var command = new SqlCommand(query, connection))
                     using (var reader = command.ExecuteReader())
                     {
-                        var databases = new List<DatabaseInfo>();
-
+                       
                         while (reader.Read())
                         {
-                            databases.Add(new DatabaseInfo
+                            var info = new DatabaseInfo
                             {
                                 DatabaseName = reader["DatabaseName"].ToString()
-                            });
-                        }
+                                };
 
-                        return databases;
+                            if (includeDetails)
+                            {
+                                if (includeStatus)
+                                {
+                                    info.DatabaseStatus = reader["Status"].ToString();
+                                    info.RecoveryModel = reader["RecoveryModel"].ToString();
+                                }
+
+                                if (includeSize)
+                                {
+                                    info.SizeMB = Convert.ToDecimal(reader["SizeMB"]);
+                                    info.FileName = reader["FileName"].ToString();
+                                    info.FileLocation = reader["FileLocation"].ToString();
+                                }
+
+                                if (includeBackups)
+                                {
+                                    info.LastFullBackup = reader["LastFullBackup"] != DBNull.Value ? reader["LastFullBackup"].ToString() : null;
+                                    info.LastLogBackup = reader["LastLogBackup"] != DBNull.Value ? reader["LastLogBackup"].ToString() : null;
+                                }
+                            }
+                        databases.Add(info);
+                        }
                     }
+                    return databases;
                 }
                 catch (Exception ex)
                 {
@@ -154,75 +213,6 @@ DBCC SHRINKFILE (N'{databaseFile}', {shrinkInt}, TRUNCATEONLY);";
                 {
                     throw new Exception($"Error al ejecutar el comando SQL: {ex.Message}", ex);
                 }
-            }
-        }
-
-        public static List<DatabaseInfo> GetDatabaseInfo(string instanceName, DAL.Contracts.IDatabaseConnectionStrategy connectionStrategy)
-        {
-            var connectionContext = new DatabaseConnectionContext(connectionStrategy);
-            List<DatabaseInfo> databaseList = new List<DatabaseInfo>();
-
-            using (SqlConnection connection = connectionContext.GetConnection(instanceName))
-            {
-                try
-                {
-                    string query = @"
-                SELECT 
-                    db.name AS DatabaseName,
-                    db.state_desc AS Status,
-                    db.recovery_model_desc AS RecoveryModel,
-                    SUM(mf.size * 8 / 1024) AS SizeMB,
-                    mf.name AS FileName,
-                    mf.physical_name AS FileLocation,
-                    (SELECT MAX(backup_finish_date) 
-                     FROM msdb.dbo.backupset 
-                     WHERE backupset.database_name = db.name AND backupset.type = 'D') AS LastFullBackup,
-                    (SELECT MAX(backup_finish_date) 
-                     FROM msdb.dbo.backupset 
-                     WHERE backupset.database_name = db.name AND backupset.type = 'L') AS LastLogBackup
-                FROM 
-                    sys.databases db
-                LEFT JOIN 
-                    sys.master_files mf ON db.database_id = mf.database_id
-                WHERE db.database_id > 4
-                GROUP BY 
-                    db.name, db.state_desc, db.recovery_model_desc, mf.name, mf.physical_name
-                ORDER BY 
-                    db.name;
-            ";
-
-                    connection.Open();
-
-                    using (SqlCommand result = new SqlCommand(query, connection))
-                    {
-                        using (var reader = result.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                var databaseInfo = new DatabaseInfo
-                                {
-                                    DatabaseName = reader["DatabaseName"].ToString(),
-                                    DatabaseStatus = reader["Status"].ToString(),
-                                    RecoveryModel = reader["RecoveryModel"].ToString(),
-                                    SizeMB = Convert.ToInt32(reader["SizeMB"]),
-                                    FileName = reader["FileName"].ToString(),
-                                    FileLocation = reader["FileLocation"].ToString(),
-                                    LastFullBackup = reader["LastFullBackup"] != DBNull.Value ? reader["LastFullBackup"].ToString() : null,
-                                    LastLogBackup = reader["LastLogBackup"] != DBNull.Value ? reader["LastLogBackup"].ToString() : null
-                                };
-
-                                databaseList.Add(databaseInfo);
-                            }
-                        }
-                    }
-
-                    return databaseList;
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception($"No se pudo conectar a la instancia {instanceName}: {ex.Message}\n Detalles: {ex.StackTrace}");
-                }
-
             }
         }
 
